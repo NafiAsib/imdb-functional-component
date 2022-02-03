b219f5a4e27e793733c99b96d06858ed57222867
NafiAsib <nafi.asib@gmail.com>
2022-02-03 16:01:17 +0600

    converted filter

--- src/components/common/filter.component.js
+++ src/components/common/filter.component.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Filter extends Component {
-    state = {};
-    render() {
-        const { items, selectedGenre, onFilter } = this.props;
-        return (
-            <div className="list-group">
-                {items.map((item) => {
-                    return (
-                        <button
-                            key={item}
-                            type="button"
-                            className={
-                                selectedGenre === item
-                                    ? 'list-group-item list-group-item-action active'
-                                    : 'list-group-item list-group-item-action'
-                            }
-                            onClick={() => onFilter(item)}
-                        >
-                            {item}
-                        </button>
-                    );
-                })}
-            </div>
-        );
-    }
-}
+const Filter = ({ items, selectedGenre, onFilter }) => {
+    return (
+        <div className="list-group">
+            {items.map((item) => {
+                return (
+                    <button
+                        key={item}
+                        type="button"
+                        className={
+                            selectedGenre === item
+                                ? 'list-group-item list-group-item-action active'
+                                : 'list-group-item list-group-item-action'
+                        }
+                        onClick={() => onFilter(item)}
+                    >
+                        {item}
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
 
 export default Filter;
