8261620ee70d1bb71e65eac206a4203a01f7974f
NafiAsib <nafi.asib@gmail.com>
2022-02-03 16:03:10 +0600

    converted pagination

--- src/components/common/pagination.component.js
+++ src/components/common/pagination.component.js
@@ -1,80 +1,73 @@
+import React from 'react';
+
 import _ from 'lodash';
 
-import React, { Component } from 'react';
+const Pagination = ({ totalItems, itemsPerPage, activePage, onClickPage }) => {
+    if (totalItems <= itemsPerPage) return null;
 
-class Pagination extends Component {
-    state = {};
+    const totalPage = Math.ceil(totalItems / itemsPerPage);
+    const pages = _.range(1, totalPage + 1, 1);
 
-    render() {
-        const { totalItems, itemsPerPage, activePage, onClickPage } =
-            this.props;
-
-        if (totalItems <= itemsPerPage) return null;
-
-        const totalPage = Math.ceil(totalItems / itemsPerPage);
-        const pages = _.range(1, totalPage + 1, 1);
-
-        return (
-            <>
-                <nav>
-                    <ul
-                        className="pagination flex-wrap justify-content-center"
-                        style={{ gap: '2px' }}
+    return (
+        <>
+            <nav>
+                <ul
+                    className="pagination flex-wrap justify-content-center"
+                    style={{ gap: '2px' }}
+                >
+                    <li
+                        className={
+                            activePage === 1
+                                ? 'page-item disabled'
+                                : 'page-item'
+                        }
                     >
-                        <li
-                            className={
-                                activePage === 1
-                                    ? 'page-item disabled'
-                                    : 'page-item'
-                            }
+                        <button
+                            className="page-link"
+                            onClick={() => onClickPage(activePage - 1)}
                         >
-                            <button
-                                className="page-link"
-                                onClick={() => onClickPage(activePage - 1)}
-                            >
-                                Previous
-                            </button>
-                        </li>
+                            Previous
+                        </button>
+                    </li>
 
-                        {pages.map((pageNumber) => {
-                            return (
-                                <li
-                                    key={pageNumber}
-                                    className={
-                                        pageNumber === activePage
-                                            ? 'page-item active'
-                                            : 'page-item'
-                                    }
+                    {pages.map((pageNumber) => {
+                        return (
+                            <li
+                                key={pageNumber}
+                                className={
+                                    pageNumber === activePage
+                                        ? 'page-item active'
+                                        : 'page-item'
+                                }
+                            >
+                                <button
+                                    className="page-link"
+                                    onClick={() => onClickPage(pageNumber)}
                                 >
-                                    <button
-                                        className="page-link"
-                                        onClick={() => onClickPage(pageNumber)}
-                                    >
-                                        {pageNumber}
-                                    </button>
-                                </li>
-                            );
-                        })}
+                                    {pageNumber}
+                                </button>
+                            </li>
+                        );
+                    })}
 
-                        <li
-                            className={
-                                activePage === pages.length
-                                    ? 'page-item disabled'
-                                    : 'page-item'
-                            }
+                    <li
+                        className={
+                            activePage === pages.length
+                                ? 'page-item disabled'
+                                : 'page-item'
+                        }
+                    >
+                        <button
+                            className="page-link"
+                            onClick={() => onClickPage(activePage + 1)}
                         >
-                            <button
-                                className="page-link"
-                                onClick={() => onClickPage(activePage + 1)}
-                            >
-                                Next
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
-            </>
-        );
-    }
-}
+                            Next
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+};
 
 export default Pagination;
