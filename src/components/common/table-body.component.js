b7451a984ef8587e70a0f7e46845e67ca18a7e46
NafiAsib <nafi.asib@gmail.com>
2022-02-03 16:04:02 +0600

    converted table-body

--- src/components/common/table-body.component.js
+++ src/components/common/table-body.component.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TableBody extends Component {
-    render() {
-        const { items, columns } = this.props;
-
-        return (
-            <tbody>
-                {items.map((item) => {
-                    return (
-                        <tr key={item.id}>
-                            {columns.map((data) => (
-                                <React.Fragment
-                                    key={item.id + ' ' + data.label}
-                                >
-                                    {data.content(item, data.key)}
-                                </React.Fragment>
-                            ))}
-                        </tr>
-                    );
-                })}
-            </tbody>
-        );
-    }
-}
+const TableBody = ({ items, columns }) => {
+    return (
+        <tbody>
+            {items.map((item) => {
+                return (
+                    <tr key={item.id}>
+                        {columns.map((data) => (
+                            <React.Fragment key={item.id + ' ' + data.label}>
+                                {data.content(item, data.key)}
+                            </React.Fragment>
+                        ))}
+                    </tr>
+                );
+            })}
+        </tbody>
+    );
+};
 
 export default TableBody;
