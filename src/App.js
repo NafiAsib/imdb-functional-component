5dab650b77a8cc554e11c9c44e7ed254cdcf4018
NafiAsib <nafi.asib@gmail.com>
2022-02-03 15:22:50 +0600

    converted App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
+
 import Movies from './components/movies.component';
 import Navbar from './components/navbar.component';
 
-class App extends React.Component {
-    state = {};
-
-    render() {
-        return (
-            <>
-                <Navbar />
-                <Movies />
-            </>
-        );
-    }
-}
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Movies />
+        </>
+    );
+};
 
 export default App;
